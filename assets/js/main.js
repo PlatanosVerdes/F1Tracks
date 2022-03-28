707ec2ec04eb9688f1fcdf6e828cdd145e8d5031
AlbertoYang1 <alberto.24yang@gmail.com>
2022-03-28 21:56:36 +0200

    28/03/2022 update
    Little update

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,3 +172,20 @@
 
 
 })()
+
+$('.carousel[data-type="multi"] .item').each(function() {
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i = 0; i < 3; i++) {
+    next = next.next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
