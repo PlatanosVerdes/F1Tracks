ad93a8335a2fffeedec845cde9042e85a3d308c9
JorgeGonzalezPascual <61434203+JorgeGonzalezPascual@users.noreply.github.com>
2022-04-19 20:25:57 +0200

    json

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,3 +14,24 @@ function leerxml() {
     if (req.status == 200)
         dump(req.responseText);
 }
+
+/* API TWITTER ENSEÑAR 10 TWITTS MÁS RECIENTES */
+const Twit = require('twit')
+
+const T = new Twit({
+    apiKey: '',
+    apiSecret: '',
+    accessToken: '',
+    accessTokenSecret: '',
+    timeout_ms:           60*1000,
+    strictSSL:            true,
+});
+
+(async () =>{
+    T.get('search/tweets', { q: '#F1 since:2020-07-11', count: 10 }, function(err, data, response) {
+        const tweets = data.statuses
+        console.log(data)
+    })
+});
+
+
