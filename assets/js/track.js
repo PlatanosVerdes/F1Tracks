ab86801d30edaa64b8b914eb2cfa031d5cd7fa1c
JorgeGonzalezPascual <61434203+JorgeGonzalezPascual@users.noreply.github.com>
2022-04-27 20:19:53 +0200

    update track

--- assets/js/track.js
+++ assets/js/track.js
@@ -3,113 +3,6 @@ console.log(idTrack);
 
 var nomTrack = idTrack;
 
-function printTrackMainInfo() {
-
-    var data
-    const xhttp = new XMLHttpRequest();
-    xhttp.open('GET', 'f1tracks.json', true);
-    xhttp.send(null);
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-
-            data = JSON.parse(this.responseText);
-            console.log(data);
-
-            let tracks = data.track;
-
-            var i;
-            for (i = 0; i<tracks.length;i++){   
-        
-                if(tracks[i].identifier==nomTrack){
-                    break;
-                }
-            }
-            var idPilot = tracks[i].datos_extra.lapRecord.pilot;
-            console.log(idPilot);
-            var pilot = getPilot(data, idPilot)
-
-            var years = getYears(tracks[i]);
-
-            let MainInfoTrack = document.createElement("div");
-                MainInfoTrack.setAttribute("class", "trackView");
-                MainInfoTrack.setAttribute("id", "trackView");
-        
-                MainInfoTrack.innerHTML = `
-                <div class="title-track">
-                <div class="row ">
-                    <div class="col">
-                            <i class="bi bi-star-fill"></i>
-                            <h10>${tracks[i].alternateName}</h10>
-                    </div>
-                </div>
-                </div>
-                <img class="img-fluid rounded mb-3 mb-md-0" src="assets/img/tracks/${tracks[i].image[0]}" alt="Track">
-
-                <div class="title">
-                    <div class="sub-title-track">
-                        <h1>${tracks[i].name}</h1>
-                    </div>
-                </div>
-                
-                <div class="detail">
-                            <div class="row align-items-start">
-                                <div class="col">
-                                    <div class="detail-info">
-                                        <div class="detail-info-title">País</div>
-                                        <div class="detail-info-content">${tracks[i].GeoCoordinates.addressCountry}</div>
-                                    </div>
-
-                                    <div class="detail-info">
-                                        <div class="detail-info-title">Distancia</div>
-                                        <div class="detail-info-content">${tracks[i].datos_extra.trackDistance} km</div>
-                                    </div>
-                                </div>
-                                <div class="col">
-                                    <div class="detail-info">
-                                        <div class="detail-info-title">Vueltas</div>
-                                        <div class="detail-info-content">${tracks[i].datos_extra.numberLaps}</div>
-                                    </div>
-                                    <div class="detail-info">
-                                        <div class="detail-info-title">Años</div>
-                                        <div class="detail-info-content">${years}</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row align-items-center">
-                                <div class="col">
-                                    <div class="detail-info">
-                                        <div class="row">
-                                            <div class="col">
-                                                <div class="detail-info-title">Vuelta rápida</div>
-                                                <div class="detail-info-content">${tracks[i].datos_extra.lapRecord.time}</div>
-                                            </div>
-                                            <div class="col ">
-                                                <img class="img-fluid rounded" src="assets/img/flags/${pilot.nationality}.svg"
-                                                    alt="Flag Country">
-                                                <div class="detail-info-content">${pilot.name} ${pilot.lastName}</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="title">
-                            <h1>Descripción pista</h1>
-                        </div>
-                        <p>
-                            ${tracks[i].description}
-                        </p>
-
-                `;
-        
-        
-            let content = document.getElementById('track-content');
-            content.appendChild(MainInfoTrack);
-        }
-    }
-}
-
 function getPilot(data, idPilot){
 
     let pilot = data.participant;
@@ -121,7 +14,6 @@ function getPilot(data, idPilot){
         alternateName: null,
         nationality: null,
         memberOf: null
-
     }
 
     for (var i = 0; i < pilot.length; i++ ){
@@ -151,6 +43,57 @@ function getYears(data){
         }
     }
     return s;
+}
+
+function printTrackMainInfo() {
+
+    var data
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'f1tracks.json', true);
+    xhttp.send(null);
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            data = JSON.parse(this.responseText);
+            console.log(data);
+
+            let tracks = data.track;
+
+            var i;
+            for (i = 0; i<tracks.length;i++){   
+        
+                if(tracks[i].identifier==nomTrack){
+                    break;
+                }
+            }
+            var idPilot = tracks[i].datos_extra.lapRecord.pilot;
+            console.log(idPilot);
+            var pilot = getPilot(data, idPilot)
+            console.log(pilot)
+
+            var years = getYears(tracks[i]);
+
+            document.getElementById('alterName-track').innerHTML=`${tracks[i].alternateName}`;
+            document.getElementById('name-track').innerHTML=`${tracks[i].name}`;
+            document.getElementById('location-track').innerHTML=`${tracks[i].GeoCoordinates.addressCountry}`;
+            document.getElementById('distance-track').innerHTML=`${tracks[i].datos_extra.trackDistance}`;
+            document.getElementById('laps-track').innerHTML=`${tracks[i].datos_extra.numberLaps}`;
+            document.getElementById('years-track').innerHTML=`${tracks[i].datos_extra.numberLaps}`;
+            document.getElementById('laps-track').innerHTML=`${years}`;
+            document.getElementById('record-time-track').innerHTML=`${tracks[i].datos_extra.lapRecord.time}`;
+            let img = document.createElement("img");
+            img.setAttribute("class", "img-fluid rounded");
+            img.setAttribute("alt", "Flag Country");
+            console.log(pilot.nationality);
+            console.log("assets/img/flags/"+pilot.nationality+".svg");
+            img.setAttribute("src", "assets/img/flags/"+pilot.nationality+".svg");
+            let detailLapRecord = document.createElement("div");
+            detailLapRecord.setAttribute("class", "detail-info-content");
+            detailLapRecord.innerHTML=`${pilot.name} ${pilot.lastName}`;
+            document.getElementById('record-time-track').innerHTML=`${tracks[i].datos_extra.lapRecord.time}`;
+            
+        }
+    }
 }
 
 window.addEventListener('load', initTrack)
