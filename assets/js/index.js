e3d7b62f49d92b6608c5f098f9d385e3061c424f
AlbertoYang1 <alberto.24yang@gmail.com>
2022-05-16 17:54:30 +0200

    Estadisticas

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,7 @@
 //Funcion que se ejecuta al clicckar una imagen de un track
 function getIdTrackClick(row) {
-    
-    let img= row.firstChild.nextElementSibling.firstChild.nextElementSibling;
-    sessionStorage.setItem('idTrack', img.alt);    
+    let img = row.firstChild.nextElementSibling.firstChild.nextElementSibling;
+    sessionStorage.setItem("idTrack", img.alt);
     location.href = "track.html";
 }
 
@@ -13,14 +12,13 @@ function createTracksIndex(data) {
     let tracksAux = [];
 
     //Ordenamos los tracks por orden de fecha
-    tracks = orderTracksBy(tracks, 'date');
+    tracks = orderTracksBy(tracks, "date");
 
-    let trackList = document.getElementById('track-list');
-    trackList.innerHTML = '';
+    let trackList = document.getElementById("track-list");
+    trackList.innerHTML = "";
     for (var i = 0; i < tracks.length; i++) {
-
         /* Si ya han pasado se printean al final */
-        if ((new Date(tracks[i].date) - currentDate()) < 0) {
+        if (new Date(tracks[i].date) - currentDate() < 0) {
             /* Guardar */
             tracksAux.push(tracks[i]);
         } else {
@@ -32,13 +30,13 @@ function createTracksIndex(data) {
     //Si hay elementos:
     if (tracksAux.length > 0) {
         //Añadir boton al Hero
-        let btnHero = document.getElementById('btn-hero');
+        let btnHero = document.getElementById("btn-hero");
         btnHero.innerHTML = `
             <a href="#col-track-list" class="btn-get-started scrollto">Próximo circuito</a>
             <a href="#title-old-tracks" class="btn-get-started scrollto">Circuitos anteriores</a>
         `;
 
-        let content = document.getElementById('page-content');
+        let content = document.getElementById("page-content");
 
         /* AÑADIMOS UNA NUEVA FILA */
         let title = document.createElement("div");
@@ -68,8 +66,8 @@ function createTracksIndex(data) {
         content.appendChild(rowOldTracks);
 
         /* AÑADIMOS LOS TRACKS ANTERIORES */
-        let trackListOld = document.getElementById('track-list-old');
-        trackListOld.innerHTML = '';
+        let trackListOld = document.getElementById("track-list-old");
+        trackListOld.innerHTML = "";
         for (var i = 0; i < tracksAux.length; i++) {
             printTrackAlternateName(tracksAux[i], trackListOld);
         }
@@ -80,7 +78,7 @@ function createMenuCircuitsIndex(year, data) {
     let index = document.getElementById("years");
     indexYear = currentDate().getFullYear() - year;
     index.innerHTML = ``;
-    for (let i = 1; i < (year + 1); i++) {
+    for (let i = 1; i < year + 1; i++) {
         let li = document.createElement("li");
         li.setAttribute("id", "years");
         li.setAttribute("title", indexYear + i);
@@ -97,49 +95,66 @@ function createMenuCircuitsIndex(year, data) {
         index.appendChild(li);
     }
 
-    document.getElementById("bt-az").addEventListener("click", () => ordenarBy("name", data));
-    document.getElementById("bt-favs").addEventListener("click", () => ordenarBy("favs", data));
+    document
+        .getElementById("bt-az")
+        .addEventListener("click", () => ordenarBy("name", data));
+    document
+        .getElementById("bt-favs")
+        .addEventListener("click", () => ordenarBy("favs", data));
 }
 
-window.addEventListener('load', initIndex)
+window.addEventListener("load", initIndex);
 async function initIndex() {
-    let navClicked = sessionStorage.getItem('navClicked');
+    let navClicked = sessionStorage.getItem("navClicked");
 
     let data = await fetchJSON();
     if (navClicked != null) {
-        sessionStorage.removeItem('navClicked');
+        sessionStorage.removeItem("navClicked");
         ordenarBy(navClicked, data);
-    }else{
+    } else {
         createTracksIndex(data);
     }
-    
+
     createMenuCircuitsIndex(4, data);
     let noticias = await jsonNoticias();
     carrouselNoticias(noticias);
     mapsIndex(data);
     carrouselEscuderias(data);
+    createSVG(stadisticsF1Drivers());
+
+}
+
+window.addEventListener("resize", stadisticsResize);
+function stadisticsResize() {
+    var driverStats = document.getElementById("DriverSVG");
+    driverStats.parentNode.removeChild(driverStats);
+    createSVG(stadisticsF1Drivers());
 }
 
 /*API MAPS*/
 function mapsIndex(data) {
     let listacircuitos = data.track;
     let tracks = listacircuitos;
-    tracks = orderTracksBy(tracks, 'date');
-    var map = L.map('map1').setView([29.422157492346255, -12.87843904797674], 1);
+    tracks = orderTracksBy(tracks, "date");
+    var map = L.map("map1").setView([29.422157492346255, -12.87843904797674], 1);
     const bounds = [
         [-74.72371974567177, -210], // [west, south]
-        [82.25330477102183, 250]  // [east, north]
+        [82.25330477102183, 250], // [east, north]
     ];
     map.setMaxBounds(bounds);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: "Global Map",
-        maxZoom: 18,
-        minZoom: 1,
-        id: 'mapbox/satellite-v9',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1IjoicGF1aW5kYW5pY29sYXUiLCJhIjoiY2wyazhmZm80MGF5cDNicGtlazdyN3kxbyJ9.zoF8CP2CPUgGrw0U8e2_cA'
-    }).addTo(map);
+    L.tileLayer(
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+        {
+            attribution: "Global Map",
+            maxZoom: 18,
+            minZoom: 1,
+            id: "mapbox/satellite-v9",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken:
+                "pk.eyJ1IjoicGF1aW5kYW5pY29sYXUiLCJhIjoiY2wyazhmZm80MGF5cDNicGtlazdyN3kxbyJ9.zoF8CP2CPUgGrw0U8e2_cA",
+        }
+    ).addTo(map);
 
     for (var i = 0; i < listacircuitos.length; i++) {
         let coordenadas = listacircuitos[i].GeoCoordinates;
@@ -147,7 +162,8 @@ function mapsIndex(data) {
         let latitud = coordenadas.latitude;
 
         var marker = L.marker([latitud, longitud]).addTo(map);
-        marker.bindPopup(`<b>${listacircuitos[i].name}</b><br>${listacircuitos[i].alternateName}<br><img src="assets/img/flags/${listacircuitos[i].datos_extra.flag}" width="30px" hight="30px" style="border:1px solid black;">`
+        marker.bindPopup(
+            `<b>${listacircuitos[i].name}</b><br>${listacircuitos[i].alternateName}<br><img src="assets/img/flags/${listacircuitos[i].datos_extra.flag}" width="30px" hight="30px" style="border:1px solid black;">`
         );
     }
 }
@@ -155,7 +171,7 @@ function mapsIndex(data) {
 /*Carrousel Escuderias*/
 function carrouselEscuderias(data) {
     let esc = data.organitation;
-    let escList = document.getElementById('carousel-inner');
+    let escList = document.getElementById("carousel-inner");
     escList.innerHTML += ``;
 
     for (var i = 0; i < esc.length; i++) {
@@ -175,44 +191,50 @@ function carrouselEscuderias(data) {
     }
 }
 
-async function jsonNoticias(){
-    const response = await fetch('https://api.currentsapi.services/v1/search?' +
-    'keywords=Formula One&page_size=10&'+ 
-    'apiKey=N59WCalcBtNE7Nu2xDtbxkC_BaUtplDjUmTOz0bGWRBC9W9Y');
+async function jsonNoticias() {
+    const response = await fetch(
+        "https://api.currentsapi.services/v1/search?" +
+        "keywords=Formula One&page_size=10&" +
+        "apiKey=N59WCalcBtNE7Nu2xDtbxkC_BaUtplDjUmTOz0bGWRBC9W9Y"
+    );
     if (!response.ok) {
         const message = `An error has occured: ${response.status}`;
         throw new Error(message);
     }
     const data = await response.json();
-    return data;  
+    return data;
 }
 
 /*Carrousel noticias*/
 function carrouselNoticias(data) {
-    let notList = document.getElementById('carousel-noticias-content');
+    let notList = document.getElementById("carousel-noticias-content");
     notList.innerHTML += ``;
     for (var i = 0; i < data.news.length; i++) {
         let date = new Date(data.news[i].published);
         if (i == 0 && data.news[i].image != "None") {
             notList.innerHTML += `
                 <div class="carousel-item active" data-bs-interval="9000">
-                <img src="${data.news[i].image}" class="d-block w-100" alt="${data.news[i].title}">
+                <img src="${data.news[i].image}" class="d-block w-100" alt="${data.news[i].title
+                }">
                 <div class="overlay">                        
                         <div class="text">
-                            <a href="${data.news[i].url}" target="_blank">${data.news[i].title}</a>
+                            <a href="${data.news[i].url}" target="_blank">${data.news[i].title
+                }</a>
                             
                             <div>${date.getFullYear()}-${date.getMonth()}-${date.getDate()}</div>
                         </div>
                     </div>
                 </div>
             `;
-        } else if(data.news[i].image != "None"){
+        } else if (data.news[i].image != "None") {
             notList.innerHTML += `
                 <div class="carousel-item" data-bs-interval="5000">
-                <img src="${data.news[i].image}" class="d-block w-100" alt="${data.news[i].title}">
+                <img src="${data.news[i].image}" class="d-block w-100" alt="${data.news[i].title
+                }">
                 <div class="overlay">                        
                         <div class="text">
-                            <a href="${data.news[i].url}" target="_blank">${data.news[i].title}</a>
+                            <a href="${data.news[i].url}" target="_blank">${data.news[i].title
+                }</a>
                             
                             <div>${date.getFullYear()}-${date.getMonth()}-${date.getDate()}</div>
                         </div>
@@ -223,15 +245,79 @@ function carrouselNoticias(data) {
     }
 }
 
-function stadisticsF1Drivers(data){
-    console.log(data);
-    var standing = data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-    console.log(standing);
+function stadisticsF1Drivers() {
+    driverStands = JSON.parse(sessionStorage.getItem("driverStanding"));
+    var standing =
+        driverStands.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    let standingsInfo = [];
+
+    for (var i = 0; i < standing.length; i++) {
+        var DriverStats = {
+            driver: standing[i].Driver,
+            points: standing[i].points,
+        };
+        standingsInfo.push(DriverStats);
+    }
+    return standingsInfo;
 }
 
-function stadisticsF1Constructor(data){
+function createSVG(data) {
+    var xmlns = "http://www.w3.org/2000/svg";
+
+    var win = window,
+        doc = document,
+        docElem = doc.documentElement,
+        body = doc.getElementsByTagName("body")[0],
+        x = win.innerWidth || docElem.clientWidth || body.clientWidth,
+        y = win.innerHeight || docElem.clientHeight || body.clientHeight;
+
+    var w;
+    if (x <= 750) {
+        w = parseInt(x * 0.9);
+    } else {
+        w = parseInt(x * (1 / 2));
+    }
+    var h = 1000;
+    var maxPoints = (w / data[0].points) * 0.7;
+
+    let driverStats = document.getElementById("estadisticas");
+    var svgElem = document.createElementNS(xmlns, "svg");
+    svgElem.setAttributeNS(null, "id", "DriverSVG");
+    svgElem.setAttributeNS(null, "viewBox", "0 0 " + w + " " + h);
+    svgElem.setAttributeNS(null, "width", w);
+    svgElem.setAttributeNS(null, "height", h);
+    svgElem.style.display = "block";
+
+    for (var i = 0; i < data.length; i++) {
+        var bar = document.createElementNS(xmlns, "g");
+        svgElem.appendChild(bar);
+        bar.setAttributeNS(null, "transform", "translate(160,20)");
+
+        var recta = document.createElementNS(xmlns, "rect");
+        recta.setAttribute("class", "bar");
+        recta.setAttribute("x", "10");
+        recta.setAttribute("width", data[i].points * maxPoints);
+        recta.setAttribute("y", 5 + i * 40);
+        recta.setAttribute("height", "30");
+        bar.appendChild(recta);
+
+        var text = document.createElementNS(xmlns, "text");
+        text.setAttributeNS(null, "transform", "translate(5,40)");
+        text.setAttributeNS(null, "style", "fill: #ffffff;")
+        svgElem.appendChild(text);
+        text.setAttributeNS(null, "y", 5 + i * 40);
+        text.setAttributeNS(null, "x", "1");
+        text.innerHTML = `${i + 1} ${data[i].driver.givenName} ${data[i].driver.familyName}`;
+
+        driverStats.appendChild(svgElem);
+    }
+}
+
+
+function stadisticsF1Constructor(data) {
     console.log(data);
-    var standing = data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
+    var standing =
+        data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
     console.log(standing);
 }
 
@@ -241,9 +327,14 @@ async function F1DriverStanding() {
         redirect: "follow",
     };
 
-    await fetch("https://ergast.com/api/f1/2022/driverStandings.json", requestOptions)
+    await fetch(
+        "https://ergast.com/api/f1/2022/driverStandings.json",
+        requestOptions
+    )
         .then((response) => response.json())
-        .then((result) => stadisticsF1Drivers(result))
+        .then((result) => {
+            sessionStorage.setItem("driverStanding", JSON.stringify(result));
+        })
         .catch((error) => console.log("error", error));
 }
 
@@ -253,7 +344,10 @@ async function F1ConstructorStanding() {
         redirect: "follow",
     };
 
-    await fetch("https://ergast.com/api/f1/2022/constructorStandings.json", requestOptions)
+    await fetch(
+        "https://ergast.com/api/f1/2022/constructorStandings.json",
+        requestOptions
+    )
         .then((response) => response.json())
         .then((result) => stadisticsF1Constructor(result))
         .catch((error) => console.log("error", error));
